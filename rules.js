47915d06c5a3a1fd12d719aa618cfd70e3ec2fd6
Ryan England <raenglandwork@gmail.com>
2024-04-18 00:00:52 -0700

    Finished with the Part(b) assignment

--- rules.js
+++ rules.js
@@ -34,14 +34,19 @@ class Location extends Scene {
         }
 
         if(locationData.Eat) {
-            this.engine.show(this.engine.fullBelly)
+            this.engine.fullBelly = true;
         }
     }
 
     handleChoice(choice) {
-        if(choice) {
-            this.engine.show("&gt; "+choice.Text);
-            this.engine.gotoScene(Location, choice.Target);
+        if (choice) {
+            if (choice.Context) {
+                this.engine.show("&gt; "+choice.Text);
+                this.engine.gotoScene(ContextSensitive, choice.Target)
+            } else {
+                this.engine.show("&gt; "+choice.Text);
+                this.engine.gotoScene(Location, choice.Target);
+            }
         } else {
             this.engine.gotoScene(End);
         }
@@ -52,6 +57,30 @@ class End extends Scene {
     create() {
         this.engine.show("<hr>");
         this.engine.show(this.engine.storyData.Credits);
+    }
+}
+
+class ContextSensitive extends Location {
+    create(key) {
+        let story = this.engine.storyData;
+        let locationData = story.Locations[key]; // TODO: use `key` to get the data object for the current story location
+        this.engine.show(locationData.Body[this.engine.item].Message); 
+        // TODO: replace this text by the Body of the location data
+
+        this.engine.item++;
+        if (this.engine.item == locationData.Length) {
+            this.engine.item = 0;
+        }
+        
+        if(locationData.Choices) { // TODO: check if the location has any Choices
+            for(let choice of locationData.Choices) { // TODO: loop over the location's Choices
+                this.engine.addChoice(choice.Text, choice);
+                // TODO: use the Text of the choice
+                // TODO: add a useful second argument to addChoice so that the current code of handleChoice below works
+            }
+        } else {
+            this.engine.addChoice("The end.")
+        }
     }
 }
 
